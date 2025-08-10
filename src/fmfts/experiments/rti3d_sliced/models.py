8394f3a254fd02539048ae1a355998b9f988a015
graps1 <asdfsolider@googlemail.com>
2025-08-10 10:44:46 +0200

    code for single step + flow models for sliced RTI

--- src/fmfts/experiments/rti3d_sliced/models.py
+++ src/fmfts/experiments/rti3d_sliced/models.py
@@ -1,6 +1,9 @@
 import torch
 from fmfts.utils.unet import UNet
 from fmfts.utils.models.cfm_velocity import VelocityModel
+from fmfts.utils.models.cfm_single_step import SingleStepModel
+from fmfts.utils.models.cfm_flow import FlowModel
+
 
 
 class VelocityModelSlicedRTI3D(VelocityModel):
@@ -39,3 +42,75 @@ class VelocityModelSlicedRTI3D(VelocityModel):
             z = torch.cat([z, pos], dim=1)
 
         return self.v_net(z)
+
+class SingleStepModelSlicedRTI3D(SingleStepModel):
+    def __init__(self, 
+                 velocity_model,
+                 p0=torch.distributions.Normal(0, 1), 
+                 features=(128, 196, 256),
+                 include_timestamp=True,
+                 include_vertical_position=True):
+        super().__init__(v=velocity_model, p0=p0)
+
+        self.include_timestamp = include_timestamp
+        self.include_vertical_position = include_vertical_position
+        self.n_channels = 4 + self.include_timestamp
+        self.in_dim = 3*self.n_channels + self.include_vertical_position
+
+        self.phi_net = UNet(
+            in_channels=self.in_dim, out_channels=self.n_channels,
+            padding=("circular", "zeros"),
+            features=features,
+            nl = torch.nn.ReLU()
+        )
+
+    def phi(self, x0, y, v):
+        bs, _, width, height = x0.shape
+        z = torch.cat([x0, y, v], dim=1)
+
+        if self.include_vertical_position:
+            pos = torch.linspace(0, 1, height)
+            pos = pos.view(1, 1, 1, height)
+            pos = pos.expand(bs, 1, width, height)
+            z = torch.cat([z, pos], dim=1)
+
+        return self.phi_net(z)
+
+class FlowModelSlicedRTI3D(FlowModel):
+    def __init__(self, 
+                 velocity_model,
+                 p0=torch.distributions.Normal(0, 1), 
+                 features=(128, 196, 256),
+                 include_timestamp=True,
+                 include_vertical_position=True,
+                 time_embedding_dim = 4):
+        super().__init__(v=velocity_model, p0=p0)
+
+        self.include_timestamp = include_timestamp
+        self.include_vertical_position = include_vertical_position
+        self.time_embedding_dim = time_embedding_dim
+        self.n_channels = 4 + self.include_timestamp
+        self.in_dim = 3*self.n_channels + 2*self.time_embedding_dim + self.include_vertical_position
+
+        self.phi_net = UNet(
+            in_channels=self.in_dim, out_channels=self.n_channels,
+            padding=("circular", "zeros"),
+            features=features,
+            nl = torch.nn.ReLU()
+        )
+
+    def phi(self, x, y, tx, v, delta):
+        bs, _, width, height = x.shape
+        tx = torch.stack([torch.cos(2**k*torch.pi*tx) for k in range(self.time_embedding_dim)], dim=1)
+        tx = tx.view(bs, self.time_embedding_dim, 1, 1).expand(-1, -1, width, height)
+        delta = torch.stack([torch.cos(2**k*torch.pi*delta) for k in range(self.time_embedding_dim)], dim=1)
+        delta = delta.view(bs, self.time_embedding_dim, 1, 1).expand(-1, -1, width, height)
+        z = torch.cat([x, y, tx, v, delta], dim=1)
+
+        if self.include_vertical_position:
+            pos = torch.linspace(0, 1, height)
+            pos = pos.view(1, 1, 1, height)
+            pos = pos.expand(bs, 1, width, height)
+            z = torch.cat([z, pos], dim=1)
+
+        return self.phi_net(z)
--- src/fmfts/experiments/rti3d_sliced/train_script.py
+++ src/fmfts/experiments/rti3d_sliced/train_script.py
@@ -3,24 +3,30 @@ import torch
 import torch.optim
 from fmfts.experiments.trainer import Trainer
 from fmfts.dataloader.rti3d_sliced import DatasetSlicedRTI3D
-from fmfts.experiments.rti3d_sliced.models import VelocityModelSlicedRTI3D
+from fmfts.experiments.rti3d_sliced.models import VelocityModelSlicedRTI3D,  \
+                                                  SingleStepModelSlicedRTI3D, \
+                                                  FlowModelSlicedRTI3D
 
 
 features_velocity = (128, 196, 196, 256)
+features_single_step = (128, 196, 256)
+features_flow = (128, 196, 256)
 lr_max = 1e-4
 lr_min = 1e-5
 batch_size = 4
 At = 3/4
 dt, dy, dz = 5, 1, 1 
 include_timestamp = True
+steps_single_step = 10
+steps_flow = 5
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("modeltype", help="must be either 'velocity' or 'flow'")
+    parser.add_argument("modeltype", help="must be either 'velocity' or 'single_step' or 'flow'")
     parser.add_argument("--new", help="creates and trains a new model", action="store_true")
 
     args = parser.parse_args()
-    assert args.modeltype in [ "velocity", "flow" ]
+    assert args.modeltype in [ "velocity", "single_step", "flow" ]
 
     print(f"creating new model: {args.new}")
     torch.set_default_device("cuda")
@@ -28,20 +34,35 @@ if __name__ == "__main__":
     if args.modeltype == "velocity":
         model = VelocityModelSlicedRTI3D(features = features_velocity)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr_max)
+        training_kwargs = dict(batch_size=batch_size)
+    elif args.modeltype in ["flow", "single_step"]:
+        velocity_model = VelocityModelSlicedRTI3D(features = features_velocity)
+        velocity_model_path = "./trained_models/model_velocity.pt"
+        try:    velocity_model.load_state_dict(torch.load(velocity_model_path, weights_only=True))
+        except: raise Exception(f"couldn't load velocity model ({velocity_model_path})")
+        
+        if args.modeltype == "single_step":
+            model = SingleStepModelSlicedRTI3D(velocity_model = velocity_model, features = features_single_step)
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr_max)
+            training_kwargs = dict(batch_size=batch_size, steps=steps_single_step)
+        else:
+            model = FlowModelSlicedRTI3D(velocity_model = velocity_model, features = features_flow)
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr_max)
+            training_kwargs = dict(batch_size=batch_size, steps=steps_flow)
 
-        trainer = Trainer(
-            model_name = args.modeltype,
-            checkpoints_dir = "./checkpoints",
-            runs_dir = "./runs",
-            model_dir = "./trained_models",
-            model = model,
-            optimizer = optimizer,
-            dataset =      DatasetSlicedRTI3D(mode = "train", At=At, dt=dt, dy=dy, dz=dz, include_timestamp=include_timestamp),
-            dataset_test = DatasetSlicedRTI3D(mode = "test" , At=At, dt=dt, dy=dy, dz=dz, include_timestamp=include_timestamp),
-            lr_min = lr_min,
-            training_kwargs = dict(batch_size=batch_size),
-            load_model_if_exists = not args.new,
-            load_optimizer_if_exists = not args.new
-        )
+    trainer = Trainer(
+        model_name = args.modeltype,
+        checkpoints_dir = "./checkpoints",
+        runs_dir = "./runs",
+        model_dir = "./trained_models",
+        model = model,
+        optimizer = optimizer,
+        dataset =      DatasetSlicedRTI3D(mode = "train", At=At, dt=dt, dy=dy, dz=dz, include_timestamp=include_timestamp),
+        dataset_test = DatasetSlicedRTI3D(mode = "test" , At=At, dt=dt, dy=dy, dz=dz, include_timestamp=include_timestamp),
+        lr_min = lr_min,
+        training_kwargs = training_kwargs,
+        load_model_if_exists = not args.new,
+        load_optimizer_if_exists = not args.new
+    )
         
     trainer.loop()
